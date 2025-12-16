b88d0029f0fc9d9a22f99c9d3083da4697ba4e45
Igor Alexeenko <alexeenko.igor@gmail.com>
2025-12-16 21:29:54 +0300

    Add SSG

--- src/app/data/services/loader.ts
+++ src/app/data/services/loader.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { inject, Injectable } from "@angular/core";
+import { Observable } from "rxjs";
 
 @Injectable({
   providedIn: 'root',
@@ -10,6 +11,91 @@ export class Loader {
   static API_URL = "/"
 
   get(URL: string) {
-    return this.#http.get(`${Loader.API_URL}${URL}`);
+    return new Observable((subscriber) => {
+      subscriber.next([
+        {
+          "id": "01",
+          "role": 1,
+          "name": "Высоцкая Анжелика",
+          "dates": {
+            "2025": {
+              "12": [true, true, false, false, true, true, false, false, true, true, false, false, true, true, false, true, false, true, false, false, true, true, false, false, true, true, false, false, true, true, false]
+            }
+          }
+        },
+        {
+          "id": "02",
+          "role": 1,
+          "name": "Леонова Виктория",
+          "dates": {
+            "2025": {
+              "12": [false, false, true, true, false, false, true, true, false, false, true, true, false, false, true, false, true, false, true, true, false, false, true, true, false, false, true, true, false, false, true]
+            }
+          }
+        },
+        {
+          "id": "03",
+          "role": 3,
+          "name": "Семашко Максим",
+          "dates": {
+            "2025": {
+              "12": [false, true, true, false, false, false, false, true, false, false, false, false, false, true, true, false, true, false, false, false, true, false, true, true, false, false, false, true, true, true, false]
+            }
+          }
+        },
+        {
+          "id": "04",
+          "role": 3,
+          "name": "Меркурьев Максим",
+          "dates": {
+            "2025": {
+              "12": [true, false, true, false, false, false, true, false, true, true, true, false, true, false, false, true, false, true, true, true, false, true, true, true, false, false, false, false, false, false, false]
+            }
+          }
+        },
+        {
+          "id": "05",
+          "role": 2,
+          "name": "Алексеенко Игорь",
+          "dates": {
+            "2025": {
+              "12": [false, true, true, false, false, true, true, false, true, true, false, true, true, true, false, true, false, false, true, true, false, false, true, false, true, true, true, false, false, true, true]
+            }
+          }
+        },
+        {
+          "id": "06",
+          "role": 2,
+          "name": "Максимов Ростислав",
+          "dates": {
+            "2025": {
+              "12": [false, true, false, true, true, true, false, true, false, false, true, true, false, false, true, false, true, true, false, true, true, true, false, false, true, true, true, true, false, false, false]
+            }
+          }
+        },
+        {
+          "id": "07",
+          "role": 2,
+          "name": "Каменьков Глеб",
+          "dates": {
+            "2025": {
+              "12": [false, false, false, false, false, true, true, false, false, false, false, false, true, true, false, false, false, false, false, true, true, false, false, false, false, false, true, true, true, false, true]
+            }
+          }
+        },
+        {
+          "id": "08",
+          "role": 4,
+          "name": "Щекотов Илья",
+          "dates": {
+            "2025": {
+              "12": [false, true, true, true, true, true, false, false, true, true, true, true, true, false, false, true, true, true, true, true, false, false, true, true, true, true, true, false, false, true, true]
+            }
+          }
+        }
+      ]
+      );
+      subscriber.complete();
+    });
   }
 }
